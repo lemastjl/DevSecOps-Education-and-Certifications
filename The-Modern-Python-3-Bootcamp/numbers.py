b6f9e4055a3c32fc8bf5d86791affe7b804146ce
Jesse LeMaster <jesse94@gmail.com>
2021-04-14 21:07:05 -0400

    Refactor

--- The-Modern-Python-3-Bootcamp/numbers.py
+++ The-Modern-Python-3-Bootcamp/numbers.py
@@ -2,8 +2,9 @@
 
 for num in range(1,21):
     if num == 4 or num == 13:
-        print(f'{num} is unlucky')
+        state = 'unlucky'
     elif i % 2 == 0:
-        print(f'{num} is even')
+        state = 'even'
     else:
-        print(f'{num} is odd')
+        state = 'odd'
+    print(f'{num} is {state}')
